1543210be05bc8c9ab510e243b7d09bbc620bfbf
Andrew Burnes <andrew.burnes@gsa.gov>
2024-07-05 10:28:01 -0700

    fix: Airtable sync task call and DB query

--- src/tasks/index.js
+++ src/tasks/index.js
@@ -11,27 +11,27 @@ class TaskRunner {
 
   async syncAirtableBase() {
     console.log('Updating Users');
-    const users = await this.db.getUsers(client);
+    const users = await this.db.getUsers();
     await this.airtable.upsertUsers(users);
     console.log(`Updated ${users.length} records\n`);
 
     console.log('Updating Orgs');
-    const orgs = await this.db.getOrgs(client);
+    const orgs = await this.db.getOrgs();
     await this.airtable.upsertOrgs(orgs);
     console.log(`Updated ${orgs.length} records\n`);
 
     console.log('Updating Sites');
-    const sites = await this.db.getSites(client);
+    const sites = await this.db.getSites();
     await this.airtable.upsertSites(sites);
     console.log(`Updated ${sites.length} records\n`);
 
     console.log('Updating Domains');
-    const domains = await this.db.getDomains(client);
+    const domains = await this.db.getDomains();
     await this.airtable.upsertDomains(domains);
     console.log(`Updated ${domains.length} records\n`);
 
     console.log('Updating Org Roles');
-    const orgRoles = await this.db.getOrgRoles(client);
+    const orgRoles = await this.db.getOrgRoles();
     await this.airtable.upsertOrgRoles(orgRoles);
     console.log(`Updated ${orgRoles.length} \n`);
 
